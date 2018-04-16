0ef28a510746add0422ff2c3aca57b5bfb46d572
Ryan Kirkpatrick <ryankirkpatrick97@gmail.com>
2018-04-15 17:17:02 -0700

    Added Ctrl-Z signalling

--- simple_shell.c
+++ simple_shell.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -10,7 +11,7 @@
 
 // Simple implementation for Shell command
 // Assume all arguments are seperated by space
-// Erros are ignored when executing fgets(), fork(), and waitpid(). 
+// Erros are ignored when executing fgets(), fork(), and waitpid().
 
 /**
  * Sample session
@@ -19,41 +20,51 @@
  *   shell: ls
  *   Makefile  simple  simple_shell.c
  *   shell: exit
-**/
+ **/
+
+void quitter(){
+    exit(0);
+}
+void handler(int sig){
+    signal(SIGTSTP, quitter);
+}
+
 
 void runcommand(char* command, char** args) {
-  pid_t pid = fork();
-  if(pid) { // parent
-    	waitpid(pid, NULL, 0);
-  } else { // child
-    	execvp(command, args);
-  }
+    pid_t pid = fork();
+    if(pid) { // parent
+        waitpid(pid, NULL, 0);
+    } else { // child
+        execvp(command, args);
+    }
 }
 
 int main(){
     char line[MAX_LINE_LENGTH];
-    //printf("shell: "); 
-    while(fgets(line, MAX_LINE_LENGTH, stdin)) {
-    	// Build the command and arguments, using execv conventions.
-    	line[strlen(line)-1] = '\0'; // get rid of the new line
-    	char* command = NULL;
-    	char* arguments[MAX_TOKEN_COUNT];
-    	int argument_count = 0;
+    //printf("shell: ");
 
-    	char* token = strtok(line, " ");
-    	while(token) {
-      		if(!command) command = token;
-      		arguments[argument_count] = token;
-	      	argument_count++;
-      		token = strtok(NULL, " ");
-    	}
-    	arguments[argument_count] = NULL;
-	if(argument_count>0){
-		if (strcmp(arguments[0], "exit") == 0)
-            		exit(0);
-    		runcommand(command, arguments);
-	}
-        //printf("shell: "); 
+    signal(SIGTSTP, handler);
+    while(fgets(line, MAX_LINE_LENGTH, stdin)) {
+        // Build the command and arguments, using execv conventions.
+        line[strlen(line)-1] = '\0'; // get rid of the new line
+        char* command = NULL;
+        char* arguments[MAX_TOKEN_COUNT];
+        int argument_count = 0;
+
+        char* token = strtok(line, " ");
+        while(token) {
+            if(!command) command = token;
+            arguments[argument_count] = token;
+            argument_count++;
+            token = strtok(NULL, " ");
+        }
+        arguments[argument_count] = NULL;
+        if(argument_count>0) {
+            if (strcmp(arguments[0], "exit") == 0)
+                exit(0);
+            runcommand(command, arguments);
+        }
+        //printf("shell: ");
     }
     return 0;
 }
