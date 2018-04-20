cb01d5d9368f005f8d1c4d91ad6840eb68a493c9
Ryan Kirkpatrick <ryankirkpatrick97@gmail.com>
2018-04-20 00:17:19 -0700

    changed pipetest around to avoid race conditions. May reset.

--- pipetest.c
+++ pipetest.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(){
     if(fork() == 0){// child
-        printf("child \n");
-    }
-    else if(fork() == 0){ //second child
-        printf("grandchild \n");
+        if(fork()==0){
+            printf("grandchild \n");
+        }
+        else{
+            waitpid(-1,NULL, 0);
+            printf("child \n");
+        }
     }
     else{ //parent
+        waitpid(-1, NULL, 0);
         printf("parent \n");
     }
 
